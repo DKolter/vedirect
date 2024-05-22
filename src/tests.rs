0c95f7341fed7334d621638503a01b433dee34fc
DKolter <danielkolter157@gmail.com>
2024-05-22 13:58:08 +0200

    Adding ping test

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,4 @@
-use tokio::io::AsyncReadExt;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio_serial::SerialPortBuilderExt;
 
 use crate::VedirectReader;
@@ -8,6 +8,11 @@ async fn test_ttys0() {
     let mut serial = tokio_serial::new("/dev/ttyS0", 19200)
         .open_native_async()
         .expect("Failed to open serial port");
+
+    serial
+        .write_all(b":154\n")
+        .await
+        .expect("Failed to write ping");
 
     let mut reader = VedirectReader::new();
     loop {
