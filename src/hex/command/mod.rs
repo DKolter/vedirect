1acf23485fab9764e6e52e577470431163c2ab10
DKolter <danielkolter157@gmail.com>
2024-06-07 23:03:30 +0200

    Fix load register address

--- src/hex/command/mod.rs
+++ src/hex/command/mod.rs
@@ -34,7 +34,7 @@ impl MpptCommand {
     pub fn to_bytes(&self) -> Vec<u8> {
         match self {
             MpptCommand::ChargerMode(enable) => set_command(0x0200, &[*enable as u8]),
-            MpptCommand::LoadMode(mode) => set_command(0xEDA8, mode.to_bytes()),
+            MpptCommand::LoadMode(mode) => set_command(0xEDAB, mode.to_bytes()),
         }
     }
 }
